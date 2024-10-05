635bf3551e1d89e4eddee0bcac09cbca2fb2934e
Mgen <m@mgenware.com>
2024-10-06 01:31:03 +0800

    Update android project

--- example/android/app/src/main/kotlin/com/fluttercavalry/saf_stream_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/fluttercavalry/saf_stream_example/MainActivity.kt
@@ -2,5 +2,4 @@ package com.fluttercavalry.saf_stream_example
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity: FlutterActivity()
