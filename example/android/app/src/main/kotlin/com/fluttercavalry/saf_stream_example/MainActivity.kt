65a6c604f3409e53ece372ad6141601e0ef47c59
Mgen <m@mgenware.com>
2024-10-06 01:39:52 +0800

    Revert "Update android project"
    This reverts commit 635bf3551e1d89e4eddee0bcac09cbca2fb2934e.

--- example/android/app/src/main/kotlin/com/fluttercavalry/saf_stream_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/fluttercavalry/saf_stream_example/MainActivity.kt
@@ -2,4 +2,5 @@ package com.fluttercavalry.saf_stream_example
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+}
